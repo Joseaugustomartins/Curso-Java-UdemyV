8bce9c30b0d62dba4ad9f054652670f8a6b4a187
JOSE AUGUSTO MARTINS QUEIROZ <joseaugustomartinsqueiroz@gmail.com>
2023-03-02 09:43:58 -0300

    novo modulo

---
+++ AulaMemoria/src/application/AulaRefVsValor.java
@@ -0,0 +1,14 @@
+package application;
+
+public class AulaRefVsValor {
+
+	public static void main(String[] args) {
+		int p;
+		p = 10;
+		System.out.println(p);
+
+		// não consigo inicializar se não tiver dado algum número 
+		// para a variavel alocar 
+	}
+
+}
