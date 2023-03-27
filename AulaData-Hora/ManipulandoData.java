479a611d5358bf3a132fb4a14aa02dda22c0f51d
JOSE AUGUSTO MARTINS QUEIROZ <joseaugustomartinsqueiroz@gmail.com>
2023-03-27 09:27:01 -0300

    att

---
+++ AulaData-Hora/ManipulandoData.java
@@ -0,0 +1,27 @@
+package aplicacao;
+
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.util.Calendar;
+
+public class ManipulandoData {
+
+	public static void main(String[] args) {
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		
+		java.util.Date d = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+		
+		System.out.println(sdf.format(d));
+		
+		Calendar cal = Calendar.getInstance();
+		
+		cal.setTime(d);
+		
+		cal.add(Calendar.HOUR_OF_DAY, 4);
+		
+		d = cal.getTime();
+		
+		System.out.println(sdf.format(d));
+
+}
+}
