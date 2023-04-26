cbf49d5c421e72bb1c037f6ad4fd6e67a6d38e05
JOSE AUGUSTO MARTINS QUEIROZ <joseaugustomartinsqueiroz@gmail.com>
2023-04-26 10:08:58 -0300

    Update SavingAccount.java

--- AulaHerançaPoliformismo/src/entities/SavingAccount.java
+++ AulaHerançaPoliformismo/src/entities/SavingAccount.java
@@ -24,4 +24,9 @@ public class SavingAccount extends acount{
 	public void updateBalance() {
 		balance += balance * interestRate;
 	}
+	
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
 }
