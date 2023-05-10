bd4f1290ec202d2c354a3bd13be406e6d526f0e5
JOSE AUGUSTO MARTINS QUEIROZ <joseaugustomartinsqueiroz@gmail.com>
2023-05-10 09:05:35 -0300

    Create Company.java

---
+++ AulaHerançaPoliformismo/src/entities/Company.java
@@ -0,0 +1,29 @@
+package entities;
+
+public class Company extends TaxPayer {
+	
+	private Integer numberOfEmployess;
+
+	public Company(String name, Double anualIncome, Integer numberOfEmployess) {
+		super(name, anualIncome);
+		this.numberOfEmployess = numberOfEmployess;
+	}
+
+	public Integer getNumberOfEmployess() {
+		return numberOfEmployess;
+	}
+
+	public void setNumberOfEmployess(Integer numberOfEmployess) {
+		this.numberOfEmployess = numberOfEmployess;
+	}
+	
+	@Override
+	public Double tax() {
+		if (numberOfEmployess > 10) {
+			return getAnualIncome() * 0.14;
+		}
+		else {
+			return getAnualIncome() * 0.16;
+		}
+	}
+}
